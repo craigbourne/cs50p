b75999809ccb6b84dc0fcc4cb87ea1744d03f933
Craig Bourne <craigbourne15@gmail.com>
2024-08-07 16:47:41 +0100

    changes to extensions.py

--- 1-conditionals/problems-1/extensions.py
+++ 1-conditionals/problems-1/extensions.py
@@ -1,25 +1,28 @@
-"""
+'''
 In a file called extensions.py, implement a program that prompts the user for the name of a file and then outputs that file’s media type if the file’s name ends, case-insensitively, in any of these suffixes:
-.gif image/gif
-.jpg image/jpg
-.jpeg image/jpeg
-.png image/png
-.pdf application/pdf 
-.txt text/plain
-.zip application/zip
+
+.gif - image/gif
+.jpg - image/jpeg
+.jpeg - image/jpeg
+.png - image/png
+.pdf - application/pdf
+.txt - text/plain
+.zip - application/zip
 If the file’s name ends with some other suffix or has no suffix at all, output application/octet-stream instead, which is a common default.
-"""
+'''
 
-file = input("File name: ").split(".")[1].lower()
-image_type = f"image/{file}"
-application_type = f"application/{file}"
+# prompt the user for the name of a file
+prompt = input("File name: ").lower().strip().split(".")[-1]
 
-# match file:
-#   case "gif" | "jpg" | "jpeg" | "png":
-#     print(image_type)
-#   case "pdf" | "zip":
-#     print(application_type)
-#   case "txt":
-#     print("text/plain")
-#   case _:
-#     print("application/octet-stream")
+# outputs that file’s media type  if .gif | .jpg | .jpeg | .png | .pdf | .txt | .zip.
+if prompt == "gif" or prompt == "jpeg" or prompt == "png":
+    print(f"image/{prompt}")
+elif prompt == "jpg":
+    print("image/jpeg")
+elif prompt == "pdf" or prompt == "zip":
+    print(f"application/{prompt}")
+elif prompt == "txt":
+    print("text/plain")
+else:
+# If the file’s name ends with some other suffix or has no suffix at all, output application/octet-stream instead
+    print("application/octet-stream")
