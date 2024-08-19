76c131ca6af6b604e40d6a7d4611d8ba8249f708
Craig Bourne <craigbourne15@gmail.com>
2024-08-19 10:06:54 +0100

    updated twttr in 2-loops

--- 2-loops/problems-2/twitter.py
+++ 2-loops/problems-2/twitter.py
@@ -2,15 +2,6 @@
 Implement a program that prompts the user for a str of text and then outputs that same text but with all vowels (A, E, I, O, and U) omitted, whether inputted in uppercase or lowercase.
 """
 
-# vowels = ["a","e","i","o","u"]
-
-# prompt = input("Input: ")
-# new_string = ""
-# for letter in prompt:
-#   if letter.lower() not in vowels:
-#       new_string += letter
-# print(new_string)
-
 prompt = input("Input: ")
 no_vowels = ''
 for letter in prompt:
